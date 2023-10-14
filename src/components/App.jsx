77e5b43aea06dae2a334ec602715608dab6cd49a
AVasilaki <poseydon463@gmail.com>
2023-10-14 09:55:21 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -34,7 +34,6 @@ export class App extends Component {
   };
   render() {
     const { filter } = this.state;
-
     const { contacts } = this.state;
     const filtredContacts = contacts.filter(contact =>
       contact.name.toLowerCase().trim().includes(filter.toLowerCase().trim())
